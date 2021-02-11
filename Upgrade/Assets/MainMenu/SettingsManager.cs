fcc6e5c2855122fde09f0d4617473796e3291aa5
realkiki <74615764+realkiki@users.noreply.github.com>
2021-02-11 16:14:03 +0000

    Main Menu Tweaks
    - Added logo
    - Moved buttons around
    - Added background music globally
    - Removed Herobrine

--- Upgrade/Assets/MainMenu/SettingsManager.cs
+++ Upgrade/Assets/MainMenu/SettingsManager.cs
@@ -8,7 +8,6 @@ public class SettingsManager : MonoBehaviour
     public Toggle fullscreentoggle;
     public Dropdown resolutiondropdown;
     
-    public Dropdown vsyncdropdown;
     public Slider musicvolumeslider;
     public Button applybutton;
     public AudioSource musicsource;
@@ -20,7 +19,6 @@ public class SettingsManager : MonoBehaviour
         fullscreentoggle.onValueChanged.AddListener(delegate { onfullscreentoggle(); });
         resolutiondropdown.onValueChanged.AddListener(delegate { onresolutionchange(); });
        
-        vsyncdropdown.onValueChanged.AddListener(delegate { onvsyncchange(); });
         musicvolumeslider.onValueChanged.AddListener(delegate { onmusicvolume(); });
         resolution = Screen.resolutions;
         applybutton.onClick.AddListener(delegate { onapplybuttonclick(); });
@@ -40,10 +38,6 @@ public class SettingsManager : MonoBehaviour
         Screen.SetResolution(resolution[resolutiondropdown.value].width, resolution[resolutiondropdown.value].height, Screen.fullScreen);
     }
     
-    public void onvsyncchange()
-    {
-        QualitySettings.vSyncCount = gamesettings.vsync = vsyncdropdown.value;
-    }
     public void onmusicvolume()
     {
         musicsource.volume = gamesettings.musicvolume= musicvolumeslider.value;
@@ -57,21 +51,9 @@ public class SettingsManager : MonoBehaviour
         string jsonData = JsonUtility.ToJson(gamesettings,true);
         File.WriteAllText(Application.persistentDataPath+ "/gamesettings.json",jsonData);
         PlayerPrefs.SetFloat("musicvolume", musicvolumeslider.value);
-        PlayerPrefs.SetInt("vsync", vsyncdropdown.value);
         PlayerPrefs.Save();
     }
     public void loadsettings()
     {
-        if (PlayerPrefs.HasKey("vsync"))
-        {
-            gamesettings = JsonUtility.FromJson<Gamesettings>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
-            musicvolumeslider.value = gamesettings.musicvolume;
-
-            vsyncdropdown.value = gamesettings.vsync;
-            resolutiondropdown.value = gamesettings.resolutionindex;
-            fullscreentoggle.isOn = gamesettings.fullscreen;
-            Screen.fullScreen = gamesettings.fullscreen;
-        }
-        
     }
 }
---
+++ Upgrade/Assets/MainMenu/backgroundMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundMusic : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<backgroundMusic>().PlayMusic();
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
