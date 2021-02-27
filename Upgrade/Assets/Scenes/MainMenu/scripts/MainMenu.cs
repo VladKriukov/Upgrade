10d04fafc1e7fb6fb2c5169ed06138cdcbffe8f7
VladKriukov <vladik.kriukov@gmail.com>
2021-02-27 12:19:16 +0000

    Tweaked Dan's level

--- Upgrade/Assets/Scenes/MainMenu/scripts/MainMenu.cs
+++ Upgrade/Assets/Scenes/MainMenu/scripts/MainMenu.cs
@@ -22,14 +22,4 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
 }
