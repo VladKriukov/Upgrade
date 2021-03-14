0cc0b91097c823ed18fd158ef6aaf19375ad2a61
Cleverceiling <daniel.speight@outlook.com>
2021-03-14 11:24:59 +0000

    attempt at loadingsettings

---
+++ Upgrade/Assets/Scenes/MainMenu/scripts/loadsettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadsettings : MonoBehaviour
+{
+    SettingsManager settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settings=FindObjectOfType<SettingsManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        settings.loadsettings();
+    }
+}
