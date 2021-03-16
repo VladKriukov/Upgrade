8f56987a132cddcafca754e36f80a831a181ab04
VladKriukov <vladik.kriukov@gmail.com>
2021-03-16 09:49:10 +0000

    Fixed load settings on update

--- Upgrade/Assets/Scenes/MainMenu/scripts/loadsettings.cs
+++ Upgrade/Assets/Scenes/MainMenu/scripts/loadsettings.cs
@@ -1,19 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class loadsettings : MonoBehaviour
 {
     SettingsManager settings;
-    // Start is called before the first frame update
     void Start()
     {
         settings=FindObjectOfType<SettingsManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         settings.loadsettings();
     }
 }
