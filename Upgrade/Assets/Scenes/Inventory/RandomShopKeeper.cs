be13f0c923a3e5f58fad0bc986129719a2f3e5eb
realkiki <74615764+realkiki@users.noreply.github.com>
2021-03-22 00:59:35 +0000

    Random Shop Keeper

---
+++ Upgrade/Assets/Scenes/Inventory/RandomShopKeeper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomShopKeeper : MonoBehaviour
+{
+    [SerializeField] Sprite[] shopKeeperSprites;
+    [SerializeField] Image shopKeeper;
+
+    void Start()
+    {
+        int num = Random.Range(0,shopKeeperSprites.Length);
+        shopKeeper.sprite = shopKeeperSprites[num];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
