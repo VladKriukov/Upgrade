066636d1de59488a0158f5066ffd21fd13a58c5b
VladKriukov <vladik.kriukov@gmail.com>
2021-03-23 11:42:36 +0000

    Added inventory to game

---
+++ Upgrade/Assets/Scripts/DDOL.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+// This script makes sure the GameObject attatched "Don't Destroy On Load" whenever any scene loads
+public class DDOL : MonoBehaviour
+{
+    private void Awake()
+    {
+        DDOL[] objs = FindObjectsOfType<DDOL>();
+        if (objs.Length > 1) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+}
