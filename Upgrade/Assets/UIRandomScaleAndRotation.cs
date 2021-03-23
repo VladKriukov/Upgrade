c4b098b7e3bb8caa98e44cc8fecd2a4c495814be
realkiki <74615764+realkiki@users.noreply.github.com>
2021-03-23 11:01:07 +0000

    Main Menu Update
    - added random rotation and scale for logo

---
+++ Upgrade/Assets/UIRandomScaleAndRotation.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRandomScaleAndRotation : MonoBehaviour
+{
+    float scale;
+
+    void Start()
+    {
+
+
+
+        StartCoroutine(ObjectRotate());
+        StartCoroutine(ObjectScale());
+
+
+        //-20, 20
+    }
+
+
+    void Update()
+    {
+        scale = Random.Range(0.5f, 2f);
+
+    }
+    IEnumerator ObjectRotate()
+    {
+        float timer = 0;
+        while (true)
+        {
+            float angle = Mathf.Sin(timer) * 20;
+            gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public float maxSize = 2f;
+    public float growFactor=1f;
+    public float waitTime = 5f;
+
+    IEnumerator ObjectScale()
+    {
+        float timer = 0;
+
+        while (true) // this could also be a condition indicating "alive or dead"
+        {
+            // we scale all axis, so they will have the same value, 
+            // so we can work with a float instead of comparing vectors
+            while (maxSize > transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+            // reset the timer
+
+            yield return new WaitForSeconds(waitTime);
+
+            timer = 0;
+            while (1 < transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
