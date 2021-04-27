19268978fd9f3d71a3e1e42151d8060170796c76
realkiki <74615764+realkiki@users.noreply.github.com>
2021-04-27 10:35:46 +0100

    Pause Menu
    - initial pause menu
    - added herobrine

---
+++ Upgrade/Assets/PauseHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour
+{
+    bool Paused = false;
+    [SerializeField] GameObject blur;
+    [SerializeField] GameObject UI;
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == true)
+            {
+                Time.timeScale = 1.0f;
+                blur.SetActive(false);
+                UI.SetActive(true);
+                Paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                blur.SetActive(true);
+                UI.SetActive(false);
+                Paused = true;
+            }
+        }
+    }
+}
