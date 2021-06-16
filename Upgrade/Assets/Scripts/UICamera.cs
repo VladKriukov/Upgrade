3cb5d86c68c7e02371bef60bd2b02caaba1afcc7
VladKriukov <vladik.kriukov@gmail.com>
2021-06-16 09:36:33 +0100

    Added camera controller

---
+++ Upgrade/Assets/Scripts/UICamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UICamera : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    Camera thisCamera;
+
+    private void Awake()
+    {
+        thisCamera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        thisCamera.fieldOfView = mainCamera.fieldOfView;
+    }
+}
