6b8848212504b467b41fffbbe54f6080a6a91b69
VladKriukov <vladik.kriukov@gmail.com>
2021-06-20 14:02:58 +0100

    Player can now kill enemies with the sword

---
+++ Upgrade/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private bool waitingForImput;
+    private Enemy enemy;
+    [SerializeField] float damageAmount = -10;
+    private void Update()
+    {
+        if (waitingForImput)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                enemy.enemyhealth(damageAmount);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy"))
+        {
+            enemy = collision.GetComponent<Enemy>();
+            waitingForImput = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy"))
+        {
+            enemy = null;
+            waitingForImput = false;
+        }
+    }
+}
